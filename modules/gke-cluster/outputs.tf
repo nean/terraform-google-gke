60119f835c5522cab29ac3791e2ffbb1c345b9b5
Neranjen <mvneranjen@gmail.com>
2021-04-27 12:41:05 +0530

    fix: set sensitive=true for sensitive outputs

--- modules/gke-cluster/outputs.tf
+++ modules/gke-cluster/outputs.tf
@@ -25,10 +25,12 @@ output "client_certificate" {
 
 output "client_key" {
   description = "Private key used by clients to authenticate to the cluster endpoint."
+  sensitive   = true
   value       = base64decode(google_container_cluster.cluster.master_auth[0].client_key)
 }
 
 output "cluster_ca_certificate" {
   description = "The public certificate that is the root of trust for the cluster."
+  sensitive   = true
   value       = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
 }
